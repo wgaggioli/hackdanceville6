099190f6e5658ff404f5d147c90367dcda05dd98
Will Gaggioli <will.gaggioli@vanderbilt.edu>
2014-11-08 17:19:54 -0600

    BPM module converted to SoundMaster class

--- sound_master.py
+++ sound_master.py
@@ -5,21 +5,27 @@ import json
 #import time
 #import sys
 #import redis
-#import song_loader
 
 
-def getAudioBPM():
-    #audio_files = song_loader.getAudioFiles()
-    audio_files = ['bass.wav']
-    afDict = {}
-    for af in audio_files:
-        y, sr = librosa.load(af)
-        tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr, hop_length=64)
-        #print(tempo)
-        #print(beat_frames)
-        afDict[af] = (tempo, list(beat_frames))
-    return json.dumps(afDict)
+class SoundMaster(object):
+
+    def __init__(self, audio_files=None):
+        if audio_files is None:
+            self.audio_files = ['bass.wav']
+        else:
+            self.audio_files = audio_files
+        
+    def get_audio_bpm(self):
+        afdict = {}
+        for af in self.audio_files:
+            y, sr = librosa.load(af)
+            tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr, hop_length=64)
+            #print(tempo)
+            #print(beat_frames)
+            afdict[af] = (tempo, list(beat_frames))
+        return json.dumps(afdict)
 
 if __name__ == '__main__':
-    print(getAudioBPM())
+    sm = SoundMaster()
+    print(sm.get_audio_bpm())
 
