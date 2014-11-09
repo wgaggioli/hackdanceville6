67a13909580bb696c59e63aeb0ab9bbb40d5acd9
clint hidinger <chidinger@gmail.com>
2014-11-09 13:17:13 -0500

    Updated to use json data from kinect.

--- visualization/cinder/sketch01/src/Sketch01.cpp
+++ visualization/cinder/sketch01/src/Sketch01.cpp
@@ -1,3 +1,7 @@
+
+#include <deque>
+#include "zeromq/zmq.hpp"
+
 #include "cinder/app/AppNative.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/Shader.h"
@@ -6,16 +10,21 @@
 #include "cinder/ImageIo.h"
 #include "cinder/Triangulate.h"
 #include "cinder/Utilities.h"
-
 #include "cinder/audio/Context.h"
 #include "cinder/audio/SamplePlayerNode.h"
 #include "cinder/audio/MonitorNode.h"
 #include "cinder/audio/GainNode.h"
 #include "cinder/audio/FilterNode.h"
+#include "cinder/audio/InputNode.h"
 #include "cinder/audio/Utilities.h"
+#include <cinder/Json.h>
+#include <cinder/MayaCamUI.h>
+#include <cinder/Rand.h>
 
 using namespace ci;
 using namespace ci::app;
+
+#define USE_FILE 1
 
 class Sketch01App : public AppNative {
 public:
@@ -26,11 +35,13 @@ public:
 	virtual void	fileDrop( FileDropEvent event ) override;
 	virtual void	keyDown( KeyEvent event ) override;
 	virtual void	mouseMove( MouseEvent event ) override;
+	virtual void	mouseDown( MouseEvent event ) override;
+	virtual void	mouseDrag( MouseEvent event ) override;
 	virtual void	prepareSettings( ci::app::AppBasic::Settings *settings ) override;
 
 	void drawOutline( size_t lineWidth );
 
-	CameraPersp			mCam;
+	MayaCamUI			mMayaCam;
 	gl::BatchRef		mBatch;
 	gl::TextureRef		mDepthTexture;
 	Channel32f			mChannel;
@@ -38,19 +49,49 @@ public:
 	audio::FilePlayerNodeRef		mFilePlayerNode;
 	audio::MonitorNodeRef			mMonitorNode1;
 	audio::MonitorNodeRef			mMonitorNode2;
+	audio::InputDeviceNodeRef		mInputNode;
 	audio::GainNodeRef				mGainNode;
 	audio::FilterLowPassNodeRef		mLowpassNode;
 	audio::FilterHighPassNodeRef	mHighpassNode;
+	
+	std::unique_ptr<zmq::context_t> mContext;
+	std::unique_ptr<zmq::socket_t> mSubscriber;
+	std::map<std::string, vec3> mJointMap;
+	JsonTree mJsonFileNode;
+	JsonTree::Iter mJsonFileIter;
+	ci::AxisAlignedBox3f mPointBBox;
+	bool mIsCameraInit = false;
+
+	struct Particle
+	{
+		Particle() 
+			: pos( 0.0f ), vel( 1.0f ), size( 10.0f ), alpha( 1.0f ), isDead( true )
+		{
+		}
+		vec3 pos;
+		vec3 vel;
+		float size;
+		float alpha;
+		bool isDead;
+
+	};
+	std::deque<Particle> mParticles;
+	ci::vec2 mMousePos;
 };
 
 void Sketch01App::prepareSettings( Settings* settings )
 {
 	settings->prepareWindow( Window::Format().size( 960, 720 ).title( "Sketch01" ) );
-	//settings->setFrameRate( 60.0f );
+	settings->setFrameRate( 60.0f );
 }
 
 void Sketch01App::setup()
 {
+	CameraPersp cam( mMayaCam.getCamera() );
+	cam.lookAt( vec3( 1000, 1000, 1000 ), vec3( 0 ) );
+	cam.setCenterOfInterestPoint( vec3( 0 ) );
+	mMayaCam.setCurrentCam( cam );
+
 	mChannel = Channel32f( loadImage( loadAsset( "depth.png" ) ) );
 	mDepthTexture = gl::Texture::create( mChannel );
 
@@ -63,9 +104,28 @@ void Sketch01App::setup()
 	mMonitorNode2 = audio::Context::master()->makeNode( new audio::MonitorNode() );
 	mLowpassNode = audio::Context::master()->makeNode( new audio::FilterLowPassNode() );
 	mHighpassNode = audio::Context::master()->makeNode( new audio::FilterHighPassNode() );
+	mInputNode = audio::Context::master()->createInputDeviceNode();
+
+	//mInputDeviceNode >> mLowpassNode;
+	//mInputDeviceNode >> mHighpassNode;
 
 	mLowpassNode >> mMonitorNode1;
 	mHighpassNode >> mMonitorNode2;
+
+#if ( !defined USE_FILE )
+		mContext.reset( new zmq::context_t( 1 ) );
+		mSubscriber.reset( new zmq::socket_t( *mContext, ZMQ_SUB ) );
+		//mSubscriber->connect( "tcp://172.31.252.124:5556" );
+		mSubscriber->connect( "tcp://localhost:5563" );
+		std::string channel( "dance-beat" );
+		mSubscriber->setsockopt( ZMQ_SUBSCRIBE, channel.c_str(), channel.size() );
+		int timeout = 5;
+		mSubscriber->setsockopt( ZMQ_RCVTIMEO, &timeout, sizeof( timeout ) );
+#else
+	mJsonFileNode = JsonTree( app::loadAsset( "sample_dancer-state.txt" ) );
+	mJsonFileIter = mJsonFileNode.begin();
+	mIsCameraInit = false;
+#endif
 }
 
 void Sketch01App::fileDrop( FileDropEvent event )
@@ -92,15 +152,101 @@ void Sketch01App::fileDrop( FileDropEvent event )
 
 void Sketch01App::resize()
 {
+	CameraPersp cam( mMayaCam.getCamera() );
+	cam.setPerspective( 60, getWindowAspectRatio(), 1, 1000 );
+	mMayaCam.setCurrentCam( cam );
 }
+
+
 
 void Sketch01App::update()
 {
+	JsonTree updateNode;
+	mPointBBox = ci::AxisAlignedBox3f( vec3( FLT_MAX, FLT_MAX, FLT_MAX ), vec3( -FLT_MAX, -FLT_MAX, -FLT_MAX ) );
+#if ( !defined USE_FILE )
+	if( !mSubscriber->connected() ) {
+			return;
+		}
+	zmq::message_t update;
+	mSubscriber->recv( &update );
+	std::string jsonDataStr = std::string( static_cast<char*>( update.data() ), update.size() );
+	updateNode = JsonTree( jsonDataStr );
+	
+#else
+	updateNode = *mJsonFileIter;
+	++mJsonFileIter;
+	if( mJsonFileIter == mJsonFileNode.end() ) {
+		mJsonFileIter = mJsonFileNode.begin();
+	}
+#endif
 
+	{
+		if( mJsonFileIter->hasChild( "points" ) ) {
+			//JsonTree timestampNode = mJsonFileIter->getChild( "timestamp" );
+			JsonTree pointsNode = mJsonFileIter->getChild( "points" );
+			for( auto ptIter = pointsNode.begin(), ptEnd = pointsNode.end(); ptIter != ptEnd; ++ptIter ) {
+				const std::string key = ptIter->getKey();
+				float x = ptIter->getValueAtIndex<float>( 0 );
+				float y = ptIter->getValueAtIndex<float>( 1 );
+				float z = ptIter->getValueAtIndex<float>( 2 );
+				vec3 v = vec3( x, y, z );
+				mPointBBox.include( v );
+				mJointMap[key] = v;
+			}
+		}
+	}
+
+	if( !mIsCameraInit ) {
+		ci::CameraPersp cam( mMayaCam.getCamera() );
+		vec3 centerPt = mPointBBox.getCenter();
+		cam.lookAt( centerPt - vec3( 0.0f, 0.0f, 1800.0f ), centerPt );
+		cam.setFarClip( 10000.0f );
+		cam.setNearClip( 10.0f );
+		cam.setFov( 80.0f );
+		mMayaCam.setCurrentCam( cam );
+		mIsCameraInit = true;
+
+		mParticles.resize( 100 );
+	}
+
+	size_t deadCount = 0;
+	for( auto &p : mParticles ) {
+		p.pos += p.vel * ( mMousePos.y  * 0.1f );
+		if( p.isDead || ( p.pos.z < 0.0f ) ) {
+
+			int selJointIdx = randInt() % mJointMap.size();
+			auto jtIter = mJointMap.begin();
+			std::advance( jtIter, selJointIdx );
+			p.pos = jtIter->second;
+			p.vel = vec3( 0, 0, -1.0f );
+			p.isDead = false;
+		}// end if
+	}// end for
 }
 
 void Sketch01App::draw()
 {
+	gl::clear();
+	//drawOutline( 16 );
+
+	gl::pushMatrices();
+	gl::setMatrices( mMayaCam.getCamera() );
+
+	gl::color( Color::white() );
+	for( auto const &jointPair : mJointMap ) {
+		const vec3 &pt = jointPair.second;
+		ci::gl::color( Color( 1, 1, 1 ) );
+		ci::gl::drawSphere( pt, 10.0f, 12 );
+	}
+
+	for( auto const &p : mParticles ) {
+		ci::gl::color( Color( 1, 0, 0 ) );
+		ci::gl::drawSphere( p.pos, p.size, 12 );
+	}
+
+	gl::popMatrices();
+
+#if 0
 	gl::disableDepthWrite();
 	gl::disableDepthRead();
 
@@ -124,6 +270,7 @@ void Sketch01App::draw()
 		gl::color( ColorA( 0.0, 0.0, 1.0, 0.5 ) );
 		ci::gl::drawSolidCircle( center + ivec2( 0, 0 ), radius * mMonitorNode2->getVolume() );
 	}
+#endif
 }
 
 void Sketch01App::keyDown( KeyEvent event )
@@ -152,6 +299,38 @@ void Sketch01App::mouseMove( MouseEvent event )
 {
 	if( !getWindowBounds().contains( event.getPos() ) )
 		return;
+	mMousePos = event.getPos();
 }
+
+//--------------------------------------------------------------
+void Sketch01App::drawOutline( size_t lineWidth )
+{
+#if 0 
+	gl::enableAlphaBlending( false );
+
+	{
+
+		//gl::color( Color(1,0,0) );
+		gl::ScopedTextureBind scopedTexture( mDepthTexture );
+		mOutliner.bindDistanceMap2D();
+		gl::drawSolidRect( app::getWindowBounds() );
+		mOutliner.unbindDistanceMap2D();
+	}
+
+	mOutliner.drawOutlines( lineWidth, vec4( 1.0f, 1.0f, 1.0f, 1.0f ) );
+#endif
+}
+
+void Sketch01App::mouseDown( MouseEvent event )
+{
+	mMayaCam.mouseDown( event.getPos() );
+}
+
+void Sketch01App::mouseDrag( MouseEvent event )
+{
+	mMayaCam.mouseDrag( event.getPos(), event.isLeftDown(), event.isMiddleDown(), event.isRightDown() );
+}
+
+
 
 CINDER_APP_NATIVE( Sketch01App, RendererGl )
